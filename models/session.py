c9b756c4267c96412954a02e6fd2ad90c61b4596
Donoras <donoras@gmail.com>
2021-11-08 20:08:01 +0200

    3 paskaita: added Many2one, One2many fields

--- models/session.py
+++ models/session.py
@@ -11,3 +11,6 @@ class Session(models.Model):
     start_date = fields.Date(default=fields.Date.today)
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
+
+    instructor_id = fields.Many2one('res.partner', string="Instructor", ondelete='set null')
+    course_id = fields.Many2one('openacademy.course', string="Course", ondelete='set null')

6910618fc42874623782e6fc43f27b665a8651ab
Donoras <donoras@gmail.com>
2021-11-09 19:43:18 +0200

    4 paskaita: added domain

--- models/session.py
+++ models/session.py
@@ -12,6 +12,6 @@ class Session(models.Model):
     duration = fields.Float(digits=(6, 2), help="Duration in days")
     seats = fields.Integer(string="Number of seats")
 
-    instructor_id = fields.Many2one('res.partner', string="Instructor", ondelete='set null')
+    instructor_id = fields.Many2one('res.partner', string="Instructor", ondelete='set null', domain=['|', ('instructor', '=', True), ('category_id.name', 'ilike', 'Teacher')])
     course_id = fields.Many2one('openacademy.course', string="Course", ondelete='set null')
     attendee_ids = fields.Many2many('res.partner', string="Attendees")

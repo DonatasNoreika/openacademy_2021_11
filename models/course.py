bf278a04bc145902bd3f17c37e2b0419012035e5
Donoras <donoras@gmail.com>
2021-11-15 20:12:55 +0200

    6 paskaita: 3 dalis - done

--- models/course.py
+++ models/course.py
@@ -7,7 +7,7 @@ class Course(models.Model):
     _name = "openacademy.course"
     _description = "OpenAcademy Courses"
 
-    name = fields.Char(string="Title", required=True)
+    name = fields.Char(string=_("Title"), required=True)
     description = fields.Text(string="Description")
     responsible_id = fields.Many2one('res.users', string="Responsible", ondelete='set null')
     session_ids = fields.One2many('openacademy.session', 'course_id', string="Sessions", )
